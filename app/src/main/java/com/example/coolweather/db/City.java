6911abf2fce63f2a0e9ba38320732b66b502c6ee
LHM <tony@gmail.com>
2018-06-11 17:23:29 +0800

    加入新建数据库和表的各项配置

---
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -0,0 +1,41 @@
+package com.example.coolweather.db;
+
+public class City {
+    private int cityid;
+    private  String cityName;
+    private  int cityCode;
+    private  int provinceCode;
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getCityid() {
+        return cityid;
+    }
+
+    public void setCityid(int cityid) {
+        this.cityid = cityid;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
+
---
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -0,0 +1,42 @@
+package com.example.coolweather.db;
+
+public class County {
+    private int id;
+    private  String countyName;
+    private  String weatherId;
+    private  int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
